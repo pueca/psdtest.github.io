f03e9504907c71d54703bc73101c17d29035ccee
pueca <krustachka@gmail.com>
2015-06-26 13:20:44 +0300

    added files

---
+++ css.php
@@ -0,0 +1,70 @@
+<style type="text/css">
+                    h1 {
+                
+                color: blue;
+                font-family: arial;
+                text-align: center;
+                font-size: 40;
+                padding-top: 30px;
+                
+            } 
+            .novinki {
+                color: black;
+                padding-top: 50px;
+                margin-left: 150px;
+                margin-right: 150px;
+                background-color: lightblue;
+            }
+            
+            .novinki .zaglavie {
+                color: darkblue;
+                font-size: 30;
+                
+                font-family: arial;
+            }
+            .novinki .opisanie{
+                font-style: italic;
+                color: darkblue;
+                font-size : 20;
+            }
+            .novinki .datata{
+                color: darkblue;
+            }
+            .ezik {
+                position: absolute;
+                color: black;
+                margin-left: 1080px;
+   
+            }
+            .ezik .text {
+                color: black;
+                font-size: 20;
+            }
+            .stranici{
+                display: inline;
+                font-size: 25;
+            }
+            .admin{
+                font-size: 15;
+                margin-left: 800px;
+     
+            }
+            .newz {
+                background-color: #ABC6DD ;
+                margin-left: 150px;
+                margin-right: 150px;
+                margin-top: 50px;
+            }
+            .newz .newzdate{
+                font-size: 20;
+            }
+            .newz .newztitle{
+                font-size: 35;
+            }
+            .newz .newzcontent{
+                font-style: italic;
+            }
+            .newz .newzsource{
+                font-size: 20;
+            }
+        </style>
---
+++ news_de.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+         <?php  include 'css.php' ;  ?>
+         </head>
+         
+         <body>
+<?php
+session_start();
+$id = isset($_REQUEST['id']) ? $id = $_REQUEST['id'] : '';
+$link = @mysql_connect('localhost','root','') or die('No server');
+mysql_select_db('novini1', $link) or die('No database');
+mysql_query("SET CHARACTER SET utf8");
+$query = "SELECT * FROM news WHERE id = \"$id\"";
+$news = mysql_query($query) or die("Select failed");
+
+  $ref = $_SERVER['HTTP_REFERER'];
+
+if (preg_match('/page/', $ref)) { ?>
+         <center><a href="<?php echo $ref ?>">zurück</a></center> <?php 
+}
+
+else { ?>
+         <center><a href="index_de.php">zurück</a></center> <?php
+
+}   
+
+ $newsx = mysql_fetch_array($news); ?>
+          <div class="newz">
+              <div class="newztitle"><?php  echo $newsx['title_de']; ?></br></div>
+              <div class="newzdate"><?php  echo $newsx['date_created']; ?></br></div>
+              <div class="newzcontent"><?php  echo $newsx['content_de']; ?></br></div>
+              <div class="newzsource"><?php  echo $newsx['source']; ?></br></div>
+          </div>
+        
+        </body>
+  
+</html>
